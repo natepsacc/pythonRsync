d8831d5a836dfd9e0e4a008432b7e2a707963adb
natepsacc <natespacc@gmail.com>
2024-02-15 15:54:33 -0500

    resolving

--- pysync.py
+++ pysync.py
@@ -3,28 +3,35 @@ import shutil
 import filecmp
 
 remoteIP = "0"
-remotePort = "222"
+remotePort = "2222"
 remoteUser = "nathan."
 remotePass = ""
-remoteDir = "HO-upload/"
+remoteDir = "HOME/template-upload/"
 localDir = "intake/"
-outputDir = ''
+outputDir = '/webapps////'
 
 
 def sftp():
     command = f"echo 'get -R {remoteDir}* {localDir}' | sshpass -p '{remotePass}' sftp -oPort={remotePort} {remoteUser}@{remoteIP}"
     print(os.popen(command).read())
 
-def ensure_dir(directory):
+def ensureDir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
+
+def clearDirContents(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            os.remove(os.path.join(root, file))
+        for dir in dirs:
+            shutil.rmtree(os.path.join(root, dir))
 
 
 def moveFiles(intake_dir, output_dir):
     for root, dirs, files in os.walk(intake_dir):
         relative_path = os.path.relpath(root, intake_dir)
         target_dir = os.path.join(output_dir, relative_path)
-        ensure_dir(target_dir)  
+        ensureDir(target_dir)  
         
         for file in files:
             src_file_path = os.path.join(root, file)
@@ -35,3 +42,4 @@ def moveFiles(intake_dir, output_dir):
 
 sftp()
 moveFiles(localDir, outputDir)
+clearDirContents(localDir)
